d1499806ffe60fba83f69005a0e5901f92b34283
Sarah Miller <sarahcmiller@ufl.edu>
2016-05-26 15:29:01 -0400

    index.html, server.js, app.js

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ var app = angular.module('todoApp', []);
 app.controller('TodoController', ['$scope', 'TodoFactory', function($scope, TodoFactory) {
 	$scope.newItem = '';
 	$scope.items = TodoFactory;
+	$scope.success = false;
 
 	//add item to the list
 	$scope.addItem = function() {
 		$scope.items.push($scope.newItem);
 		$scope.newItem = '';
+		$scope.success = true;
 	}
 
 	//remove item from the list
 	$scope.deleteItem = function(index) {
 		$scope.items.splice(index, 1);
 	}	
+
+	//list of items and when added
+	$scope.getAllitems = function(index) {};
 }]);
 
+app.controller('NextController', function($scope, TodoFactory) {
+	$scope.items = TodoFactory;
+})
+
+app.factory('TodoFactory', [function() {
+	var items = []
+}]);
